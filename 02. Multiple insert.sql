bc1a32b42d54c83167a8327d13fbbabbeccbd189
Abhijit Patil <72210215+abpkit@users.noreply.github.com>
2022-06-02 20:49:02 +0530

    Add files via upload

---
+++ 02. Multiple insert.sql
@@ -0,0 +1,5 @@
+insert into personal (id, name, birth_date, phone, gender)
+values
+(3, "Sachine", "1991-05-07", "234567891", "M"),
+(4, "Dada", "1990-04-05", "345678912", "M"),
+(5, "Smruti", "1989-03-04", "456789123", "F");
